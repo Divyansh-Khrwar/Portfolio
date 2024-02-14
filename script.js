c7a02eece98e373f34f99be6220769cdcc0e7121
Divyansh-Khrwar <111272080+Divyansh-Khrwar@users.noreply.github.com>
2024-02-14 20:26:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const logo = document.querySelector(".logo")
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+    logo.classList.toggle("open")
+}
+// CURSOR
+
